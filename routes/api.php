25e200a424a7a84ea34932d637af794299bae0a0
eeomts <mateolimoreira7@gmail.com>
2026-04-11 23:15:28 -0300

    atualiza desing do swagger para scalar

--- routes/api.php
+++ routes/api.php
@@ -36,20 +36,10 @@ if ($path === '/api/docs' && $_SERVER['REQUEST_METHOD'] === 'GET') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>API Notificações - Docs</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
 </head>
 <body>
-  <div id="swagger-ui"></div>
-  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
-  <script>
-    SwaggerUIBundle({
-      url: "{$specUrl}",
-      dom_id: '#swagger-ui',
-      presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset],
-      layout: "BaseLayout",
-      deepLinking: true,
-    });
-  </script>
+  <script id="api-reference" data-url="{$specUrl}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
 </body>
 </html>
 HTML;
